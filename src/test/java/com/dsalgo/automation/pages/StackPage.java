18d093e6e37f4e529108f8b1fbb00b15f66adf29
MadhaviPoreddy <mporeddy1014@gmail.com>
2025-04-28 04:23:31 -0400

    madhavi

--- src/test/java/com/dsalgo/automation/pages/StackPage.java
+++ src/test/java/com/dsalgo/automation/pages/StackPage.java
@@ -1,5 +1,97 @@
 package com.dsalgo.automation.pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import com.dsalgo.automation.driver.DriverFactory;
+//import org.openqa.selenium.JavascriptExecutor;
+
+
 public class StackPage {
 
+	WebDriver driver;
+
+	@FindBy (xpath="//a [@href='stack']") WebElement stackGetStartedBtn;
+	
+	//Operations in stack
+	@FindBy (xpath="//*[text()='Operations in Stack']") WebElement operationsInStackLink;
+	@FindBy (xpath="//a[@href='/tryEditor']") WebElement operationsInStackLinkTryEdit;
+	@FindBy (css=".CodeMirror") WebElement operationsInStackLinkTextEdit;
+	@FindBy (xpath="//button[text()='Run']") WebElement operationsInStackLinkTextEditRun;
+	
+
+	//Implementation 
+	@FindBy (xpath="//*[text()='Implementation']") WebElement stackImplementation;
+	@FindBy (xpath="//a[@href='/tryEditor']") WebElement stackImplementationTryEdit;
+	@FindBy (css=".CodeMirror") WebElement stackImplementationTextEdit;
+	@FindBy (xpath="//button[text()='Run']") WebElement stackImplementationTextEditRun;
+	
+	//Applications
+	@FindBy (xpath="//a [@href='stack-applications']") WebElement stackApplications;
+	@FindBy (xpath="//a[@href='/tryEditor']") WebElement stackApplicationsTryEdit;
+	@FindBy (css=".CodeMirror") WebElement stackApplicationsTextEdit;
+	@FindBy (xpath="//button[text()='Run']") WebElement traversalTextEditRun;
+	
+	//practice Questions
+	@FindBy (xpath="//*[text()='Practice Questions']") WebElement stackPracticeLink;
+	
+	@FindBy(xpath = "//pre[@id='output']") WebElement output;
+
+	
+	public StackPage()
+ 	{
+ 	
+		//this.driver=driver;
+ 		this.driver=DriverFactory.getDriver();
+		PageFactory.initElements( driver, this);
+ 	}
+	
+	public String verifyOutput() {
+		return output.getText();
+	}
+	
+	public void stackGetStartedBtn()
+	{
+		stackGetStartedBtn.click();
+	}
+	public String verfyPageTitle()
+	{
+ 		return driver.getTitle();
+ 
+ 	}
+	public void operationsInStackLink()
+	{
+		operationsInStackLink.click();
+	}
+	public void operationsInStackLinkTryEdit()
+	{
+		operationsInStackLinkTryEdit.click();
+	}
+	public void operationsInStackLinkTextEditRun()
+	{
+		operationsInStackLinkTextEditRun.click();
+	}
+	
+	public void stackPracticeLink()
+	{
+		stackPracticeLink.click();
+	}
+	
+	public void stackImplementation()
+	{
+		stackImplementation.click();
+	}
+	public void stackImplementationTryEdit()
+	{
+		stackImplementationTryEdit.click();
+	}
+	public void stackApplications()
+	{
+		stackApplications.click();
+	}
+	public void stackApplicationsTryEdit()
+	{
+		stackApplicationsTryEdit.click();
+	}
 }
--- src/test/java/com/dsalgo/automation/tests/ArrayPageTest.java
+++ src/test/java/com/dsalgo/automation/tests/ArrayPageTest.java
@@ -1,5 +1,80 @@
 package com.dsalgo.automation.tests;
+import java.util.List;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.xmlbeans.impl.xb.xsdschema.Public;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import com.dsalgo.automation.base.BaseClass;
+import com.dsalgo.automation.driver.DriverFactory;
+import com.dsalgo.automation.pages.*;
+import com.dsalgo.automation.tests.HomePageTest;
 
-public class ArrayPageTest {
+import com.dsalgo.automation.utils.NavigationUtil;
 
+@SuppressWarnings("unused")
+public class ArrayPageTest extends BaseClass{
+
+	public HomePageTest homeTest= new HomePageTest();
+	
+	private ArrayPage Array; 
+	
+	 // Initialize logger for this class
+   private static final Logger logger = LogManager.getLogger(ArrayPageTest.class);
+   
+   @BeforeClass
+   public void setUpPage() {
+		
+	   Array=new ArrayPage(driver); 
+   }
+	
+	@BeforeMethod
+	public void navigateBeforeEachTest() {
+	    NavigationUtil.navigateToHomePage(driver);
+	    NavigationUtil.performLogin(driver);
+		NavigationUtil.clickModuleGetStarted(driver, "Array");
+		//NavigationUtil.clickSignout(driver);	
+	}
+	
+	@AfterTest
+	public void closeBrowser() {
+        DriverFactory.quitDriver();
+    }
+
+
+	@Test
+	public void checkArrayLandingPage() {
+		Array.arrayPageHeading();
+		Assert.assertEquals(Array.verfyArrayTitle().toLowerCase(), "array", "Not Redirected to Array page as expected");
+		logger.info("Redirected to Array Landing page successfully");
+	}	
+	
+	@Test
+	public void arrayPythonLink() {
+		Array.arrayPythonLink();
+		Assert.assertEquals(Array.verfyArrayTitle(), "Arrays in Python", "Not Redirected to Array page as expected");
+		logger.info("Redirected to arrays-in-python Landing page successfully");
+	}	
+	
+	@Test
+	public void arrayPythonLinkTryEdit() {
+		Array.arrayPythonLink();
+		Array.arrayPythonLinkTryEdit();
+		Assert.assertEquals(Array.verfyArrayTitle(), "Assessment", "Not Redirected to Array page as expected");
+		logger.info("Redirected to arrays-in-python Text Editor page successfully");
+	}	
+	
+	@Test
+	public void arrayPythonLinkTryEdit() {
+		Array.arrayPythonLink();
+		Array.arrayPythonLinkTryEdit();
+		Assert.assertEquals(Array.verfyArrayTitle(), "Assessment", "Not Redirected to Array page as expected");
+		logger.info("Redirected to arrays-in-python Text Editor page successfully");
+	}	
 }
